c9d7a35c37e93dd9a39ac8e4b7f82fdb5bc18255
Maryia Tarasevich <Maryia.Tarasevich@itechart-group.com>
2022-05-27 13:58:47 +0300

    added basic structure

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Promo from '../promo/Promo';
+
+function Home() {
+    return (
+        <div className='home'>
+            <Promo />
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../nav/Nav';
+
+function Promo() {
+    return (
+        <div className='promo__wrap'>
+            <Nav />
+            
+        </div>
+    )
+}
+
+export default Promo;
